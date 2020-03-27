86160c172fac24b2e29511243520c23c98a99bd9
Yuri Faria <yuri.yurifaria@gmail.com>
2020-03-26 23:16:37 -0300

    frontend finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static")
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return send_from_directory('../static', 'index.html')
+
+@app.route('/upload')
+def upload():
+    return send_from_directory('../static', 'image.html')
+
+@app.route('/<path:path>')
+def serve(path):
+    return send_from_directory('../static', path)
+
 
 from app.controllers import default
