bcbdff09fa3b33bddfa526bb395a43db45a6f391
Yuri Faria <yuri.yurifaria@gmail.com>
2020-03-29 20:23:57 -0300

    bug repeating images fixed

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -65,7 +65,7 @@ def minning():
   values = []
 
   for i in range(pages):
-    url = "https://unsplash.com/napi/search/photos?query=" + sentence + "&xp=&per_page=20&page=" + str()
+    url = "https://unsplash.com/napi/search/photos?query=" + sentence + "&xp=&per_page=20&page=" + str(i + 1)
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
     result = requests.get(url, headers=headers)
